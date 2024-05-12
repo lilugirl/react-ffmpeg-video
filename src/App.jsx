0d863027098263994acd152625cf0d39e2295cf8
yi liu <yi7.liu@symbio.com>
2024-05-12 19:53:59 +0800

    create video

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import { useState,useEffect } from 'react'
+import { useState,useEffect,useRef } from 'react'
 import {createFFmpeg,fetchFile} from '@ffmpeg/ffmpeg'
 import './App.css'
 
 function App() {
+  const [videoSrc,setVidoSrc]=useState('')
+  // Upload file functionality
   const [loaded, setLoaded] = useState(false)
+  const [imageFile,setImageFile]=useState()
+  const [soundFile,setSoundFile]=useState()
+  const  ffmpegRef=useRef()
+  
   const load=async ()=>{
-    const ffmpeg=createFFmpeg({log:true})
+    ffmpegRef.current=createFFmpeg({log:true})
+    const ffmpeg=ffmpegRef.current
     if(!ffmpeg.isLoaded()){
        await ffmpeg.load()
        setLoaded(true)
@@ -18,17 +25,40 @@ function App() {
     }
   }
 
+
+  const handleChangeImage=(e)=>{
+    const file=e.target.files[0]
+    setImageFile(file)
+  }
+  const handleChangeSound=(e)=>{
+    const  file=e.target.files[0]
+    setSoundFile(file)
+  }
+
+  const createVideo=async()=>{
+     const ffmpeg=ffmpegRef.current
+     ffmpeg.FS('writeFile','image.png',await fetchFile(imageFile))
+     ffmpeg.FS('writeFile','sound.mp3',await fetchFile(soundFile))
+     await ffmpeg.run("-framerate","1/10","-i","image.png","-i","sound.mp3","-c:v","libx264","-t","10","-pix_fmt","yuv420p","-vf","scale=1920:1080","test.mp4")
+     const data=ffmpeg.FS('readFile','test.mp4')
+     setVidoSrc(URL.createObjectURL(new Blob([data.buffer],{type:'video/mp4'})))
+  }
+
+ 
   useEffect(()=>{
      load()
   },[])
+
+  console.log({loaded,ffmpegRef,videoSrc,imageFile,soundFile})
 
   return (
     <>
      {!loaded && <div>加载中...</div>}
      {loaded && <div>已加载</div>}
-     <input type="file"  id="image" />
-     <input type="file"  id="sound" />
-     <button>生成视频</button>
+     <video src={videoSrc} controls ></video> <br/>
+     <input type="file"  id="image" accept="image/*" onChange={handleChangeImage} />
+     <input type="file"  id="sound" accept="sound/*" onChange={handleChangeSound} />
+     <button disabled={!loaded ||  !imageFile ||  !soundFile} onClick={createVideo}>生成视频</button>
     </>
   )
 }
